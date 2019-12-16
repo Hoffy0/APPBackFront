763034a1a973dc9c7dd7f1ca4de8fe9c65dcee5c
Max <hackdarck31@gmail.com>
2019-12-15 23:38:39 -0300

    No hay cambios

--- src/passport/local-auth.js
+++ src/passport/local-auth.js
@@ -21,18 +21,17 @@ passport.use('local-signup', new LS({
     passwordField: 'password',
     passReToCallBack: true
 }, async (req, email, password, done) => {
-       const user = await User.findOne({'email': email})
-       console.log(user)
-        if(user) {
-            return done(null, false, req.flash('signupMessage', 'The Email is already Taken.'));
-        }
-        else{
+const user = await User.findOne({'email': email})
+console.log(user)
+if(user) {
+  return done(null, false, req.flash('signupMessage', 'The Email is already Taken.'));
+ } else {
     const newUser = new User();
     newUser.email = email;
     newUser.password = newUser.encryptarPass(password);
 console.log(newUser)
-    await newUser.save();
-    done(null, newUser);
-     }
+   await newUser.save();
+   done(null, newUser);
+ }
 }));
 /////////////////////////////////////////////////////////////////////
